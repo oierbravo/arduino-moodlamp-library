c4d5067b9f4ccfa83e2204edafbfb1794727cbf1
Oier Bravo Urtasun <oierbravo@gmail.com>
2013-01-31 03:07:15 +0100

    added license

--- Moodlamp.cpp
+++ Moodlamp.cpp
@@ -1,3 +1,15 @@
+/*
+  Moodlamp.h - Moodlamp library
+  Copyright (C) 2013 Oier Bravo Urtasun.  All rights reserved.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+*/
+
 #include "Arduino.h"
 #include "Moodlamp.h"
 
--- Moodlamp.h
+++ Moodlamp.h
@@ -1,3 +1,18 @@
+/*
+  Moodlamp.h - Moodlamp library
+  Copyright (C) 2013 Oier Bravo Urtasun.  All rights reserved.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+*/
+
+
+Author: Oier Bravo Urtasun - 2013
+*/
 #ifndef Moodlamp_h
 #define Moodlamp_h
 
